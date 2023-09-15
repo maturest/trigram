6e00aa01fe6169ccd8fec8cb7b74c045a104af3a
xyn-nxy <xy982744653@qq.com>
2023-09-15 17:09:07 +0800

    十二神数新增功能

--- src/TwelveGodNums.php
+++ src/TwelveGodNums.php
@@ -5,8 +5,9 @@ namespace Maturest\Trigram;
 
 use Maturest\Trigram\Traits\TwelveGodNums\BigFailScene;
 use Maturest\Trigram\Traits\TwelveGodNums\BigSuccessScene;
+use Maturest\Trigram\Traits\TwelveGodNums\PassJewelry;
 
 class TwelveGodNums
 {
-    use BigFailScene, BigSuccessScene;
+    use BigFailScene, BigSuccessScene, PassJewelry;
 }
