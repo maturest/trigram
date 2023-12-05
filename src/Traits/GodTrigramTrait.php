77dff6927f156b403d49f68cf6937a18a0160d5d
xyn-nxy <xy982744653@qq.com>
2023-12-05 10:36:08 +0800

    修复请神卦判断三

--- src/Traits/GodTrigramTrait.php
+++ src/Traits/GodTrigramTrait.php
@@ -38,8 +38,10 @@ trait GodTrigramTrait
         $res[] = $this->getWholeFortune('找回魂魄，');
         if (Str::contains($this->question, ['身体', '健康'])) $statement = ['找回元神，', '化解元神受制，'];
         else $statement = ['找回贵人助力，', '化解贵人受制，'];
-        $res[] = $this->getMagnatePower($god_wx, $statement[0]);
-        $res[] = $this->getDissolveRoad($god_wx, $statement[1]);
+        $first = $this->getGodJudgmentThree($god_wx, $statement[0]);
+        $last = $this->getDissolveRoad($god_wx, $statement[1]);
+        $res[] = $first;
+        if ($first !== $last) $res[] = $last;
 
         return $res;
     }
@@ -62,5 +64,74 @@ trait GodTrigramTrait
         $result[] = $this->firstParagraph();
         $result[] = $this->acc($god);
         return $result;
+    }
+
+
+    public function getGodJudgmentThree($god_wx, $output_statement): string
+    {
+        $judgment_one = false;
+        $wx_grow_me = $this->getWhoGrowMe($god_wx);
+        $positions = [];
+        foreach ($this->getDongYao() as $yao) {
+            if ($wx_grow_me == $this->getWxByDz($yao['dz'])) {
+                $positions[] = [
+                    'position' => $yao['column'] . $yao['row'],
+                    'is_dong' => $yao['is_dong'],
+                    'is_an_dong' => $yao['is_an_dong'],
+                    'dz' => $yao['dz'],
+                    'wx' => $this->getWxByDz($yao['dz']),
+                ];
+            }
+        }
+
+        foreach ($this->getTransDetail() as $yao) {
+            if ($wx_grow_me == $this->getWxByDz($yao['dz'])) {
+                $positions[] = [
+                    'position' => $yao['column'] . $yao['row'],
+                    'is_dong' => $yao['is_dong'],
+                    'is_an_dong' => $yao['is_an_dong'],
+                    'dz' => $yao['dz'],
+                    'wx' => $this->getWxByDz($yao['dz']),
+                ];
+            }
+        }
+
+        foreach ($positions as $position) {
+            if (
+                $this->getIsKongWangByPosition($position)
+                || $this->getIsRuByPosition($position)
+                || $this->getIsHeByPosition($position)
+            ) {
+                $judgment_one = true;
+            }
+        }
+
+        if ($judgment_one) {
+            return $output_statement;
+        } else {
+            $new_positions = [];
+            // 月令
+            if ($wx_grow_me ==  $this->getMonthWx()) {
+                $new_positions[] = [
+                    'position' => '61',
+                    'dz' => $this->getMonthDz(),
+                    'wx' => $this->getMonthWx(),
+                ];
+            }
+            // 日令
+            if ($wx_grow_me ==  $this->getDayWx()) {
+                $new_positions[] = [
+                    'position' => '62',
+                    'dz' => $this->getDayDz(),
+                    'wx' => $this->getDayWx(),
+                ];
+            }
+
+            foreach ($new_positions as $position) {
+                if ($this->getIsRuByPosition($position)) return '化解贵人受制，';
+            }
+        }
+
+        return '';
     }
 }
